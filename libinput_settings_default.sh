c6292b0e3044d02c6899bf0da5ee40482d30a249
David Karapetyan <david.karapetyan@gmail.com>
2017-08-01 22:34:48 -0400

    more changes--libinput and synclient

---
+++ libinput_settings_default.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xinput --set-prop 'SynPS/2 Synaptics TouchPad' 'libinput Tapping Enabled' 1
+xinput --set-prop 'SynPS/2 Synaptics TouchPad' 'libinput Accel Speed' 1
+xinput --set-prop 'SynPS/2 Synaptics TouchPad' 'Coordinate Transformation Matrix' 8.000000 0.000000 0.000000 0.000000 8.000000 0.000000 0.000000 0.000000 8.000000
--- synclient_settings_custom.sh
+++ synclient_settings_custom.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 synclient VertScrollDelta=60
 synclient HorizScrollDelta=60
-synclient MaxSpeed=10
+#synclient MinSpeed=2
+synclient MinSpeed=1
+synclient MaxSpeed=5
 synclient AccelFactor=0.075
-synclient SingleTapTimeout=100
-synclient ClickTime=0
+#synclient SingleTapTimeout=50
+synclient SingleTapTimeout=0
+synclient PalmDetect=1
+#synclient ClickTime=0
 
